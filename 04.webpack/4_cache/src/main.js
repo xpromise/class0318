1a5cdff958a9f2880634e7460e314960a1a410fb
xiongjian <webjsforyou@gmail.com>
2020-09-30 11:54:36 +0800

    feat: webpack优化 缓存

--- 04.webpack/4_cache/src/main.js
+++ 04.webpack/4_cache/src/main.js
@@ -1,3 +1,8 @@
 import "./index.css";
 
 console.log(process.env.NODE_ENV);
+console.log("hello cache~~~~~");
+
+import(/* webpackChunkName: "add" */"./add").then(({ default: add }) => {
+	console.log(add(1, 1));
+});
--- 04.webpack/4_cache/webpack.config.js
+++ 04.webpack/4_cache/webpack.config.js
@@ -2,6 +2,27 @@ const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+/**
+	缓存 cache
+		问题：资源被强制缓存起来了，服务器更新了资源，由于强制缓存的原因，客户端不会重新请求资源，导致资源不能及时更新
+		解决：
+			1. hash webpakc打包会生成一个hash，所有资源都共享这个值
+				优点：一旦文件发生变化，重新打包会生成新的文件名，就会让强制缓存失效，从而加载新资源
+				缺点：其中一个文件变化了，所有文件都一起变化，其他文件缓存就失效
+			2. chunkhash 根据是否是同一个chunk来生成hash值
+				问题：css由于css-loader会打包到js中，导致css和js同属于一个chunk
+				共享一个hash值。css文件变化，js文件也会变化
+			3. contenthash 会根据文件内容来生成hash值（只有文件内容发生了变化，才会变化）
+				问题：改动某个js文件（contenthash会变化），依赖这个js文件也会发生变化
+
+			4. 提取所有文件的hash成单独文件
+				optimization: {
+					runtimeChunk: {
+						name: entrypoint => `runtime~${entrypoint.name}`
+					}
+				}
+ */
+
 module.exports = {
 	// 入口文件：指示webpack从哪个文件开始打包
 	entry: "./src/main.js",
@@ -10,7 +31,7 @@ module.exports = {
 	// },
 	// 输出：打包后的文件输出到哪里去
 	output: {
-		filename: "[name].js",
+		filename: "[name].[contenthash:8].js",
 		path: resolve(__dirname, "build"),
 	},
 	module: {
@@ -32,7 +53,14 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: "./public/index.html",
 		}),
-		new MiniCssExtractPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].[contenthash:8].css",
+		}),
 	],
 	mode: "development", // process.env.NODE_ENV = 'development'
+	optimization: {
+		runtimeChunk: {
+			name: entrypoint => `runtime~${entrypoint.name}`
+		}
+	}
 };
